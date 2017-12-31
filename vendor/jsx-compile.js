e671f404d29cf221ede2079bc9febfe997a9ec6e
jskhanframework <jskhanframework@gmail.com>
2017-12-31 15:31:41 -0500

    Upando

---
+++ vendor/jsx-compile.js
@@ -0,0 +1,74 @@
+function _asyncToGenerator(fn) {
+  return function() {
+    var gen = fn.apply(this, arguments);
+    return new Promise(function(resolve, reject) {
+      function step(key, arg) {
+        try {
+          var info = gen[key](arg);
+          var value = info.value;
+        } catch (error) {
+          reject(error);
+          return;
+        }
+        if (info.done) {
+          resolve(value);
+        } else {
+          return Promise.resolve(value).then(
+            function(value) {
+              step("next", value);
+            },
+            function(err) {
+              step("throw", err);
+            }
+          );
+        }
+      }
+      return step("next");
+    });
+  };
+}
+
+var parse_jsx = jsx => {
+  let remove_whitespace = tpl => tpl.replace(/(\n|\s\s)/g, "");
+  let clean = remove_whitespace(jsx);
+  let matches = clean.match(/(?!\((.*)\)\{)\((.*?)\)\;/g);
+  let is_html = code => /\<(.*)\>(.*)\<\/(.*)\>/.test(code);
+
+  if (!Array.isArray(matches)) return;
+
+  Array.from(matches).map(pattern => {
+    if (!is_html(pattern)) return;
+    var new_v = "`" + pattern.trim().slice(1, -2) + "`";
+    if (/{{(.*)}}/gim.test(new_v)) {
+      Array.from(new_v.match(/{{(.*?)}}/g)).map(v => {
+        var nv = "${" + v.replace(/(\{\{|\}\})/gim, "").trim() + "}";
+        new_v = new_v.replace(v, nv);
+      });
+    }
+    new_v = new_v.replace(/(\s\s|\n)/gim, "");
+    clean = clean.replace(pattern, new_v);
+  }); // new_v
+
+  var rt = `
+                var exports = {},
+                    module = {exports: {}};
+                ${clean}
+                return Object.assign({}, module.exports, exports);
+          `;
+  return new Function(rt);
+};
+
+const jsx_loader = (() => {
+  var _ref = _asyncToGenerator(function*(jsx_file) {
+    let prefix = "./";
+    jsx_loader.folder_jsx = jsx_loader.folder_jsx || "jsx/";
+    var path = prefix + jsx_loader.folder_jsx + jsx_file + ".jsx";
+    var code = yield fetch(path);
+    code = yield code.text();
+    return parse_jsx(code)();
+  });
+
+  return function jsx_loader(_x) {
+    return _ref.apply(this, arguments);
+  };
+})();
